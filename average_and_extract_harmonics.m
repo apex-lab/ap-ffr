13242986558d467744736de8fc967fad53c49fac
john-veillette <johnv@uchicago.edu>
2020-07-31 10:15:21 -0500

    adds ability to select files with particular filter setting to get_file() and fixes a fieldtrip path issue. Also, now grabs all harmonics up to 1500 Hz (instead of just first seven)

--- average_and_extract_harmonics.m
+++ average_and_extract_harmonics.m
@@ -1,15 +1,19 @@
 %% filepaths and constants
 addpath('~/repos/fieldtrip')
+ft_defaults % sets paths required for fieldtrip
 addpath('functions')
-DIR = '~/Documents/AP_abr/raw_reanalyzed/piano_complex_export_sept30_2000';
+DIR = '~/Documents/ap-ffr/export/2000';
 
 % subject info
 SUBJ = 1:50;
-BAD_SUBJ = [5 19 28 39];
+BAD_SUBJ = [5 19 28 39]; 
+
+% filter info
+LP_FILTER = 2000;
 
 % condition specific info
-COND = 'piano';
-F0 = 262.67;
+COND = 'complex';
+F0 = 207.65;
 TIME_WINDOW = [0 .2]; % in seconds
 
 s = RandStream('mlfg6331_64'); % set random seed for reproducible results
@@ -21,7 +25,7 @@ allSubj_timelock = cell(1, length(subj));
 j = 1; % keeps track of where we are in allSubj_timelock
 for i = subj % for each subject
     % load their data
-    [fname_inv, fname_noninv] = get_file(i, COND, DIR);
+    [fname_inv, fname_noninv] = get_file(i, COND, DIR, LP_FILTER);
     inv = BVmat2ft_raw(fname_inv);
     noninv = BVmat2ft_raw(fname_noninv);
     % subsample from larger datafile to make trial counts equal
@@ -34,9 +38,10 @@ for i = subj % for each subject
         cfg.trials = randsample(length(noninv.trial), length(inv.trial));
         noninv = ft_selectdata(cfg, noninv);
     end
+    assert(length(inv.trial) == length(noninv.trial));
     % average over trials within inverted and noninverted
     cfg = [];
-    cfg.channel = {'EP1'};
+    cfg.channel = 'EP1';
     inv = ft_timelockanalysis(cfg, inv);
     noninv = ft_timelockanalysis(cfg, noninv);
     % and finally average the inverted with the noninverted
@@ -59,12 +64,21 @@ all_data_raw.time = time;
 all_data_raw.label = allSubj_timelock{1}.label;
 
 %% compute fourier transform and pull out power values of interest
+
+% we want all harmonics up to 1500 Hz
+foi = [F0];
+h = 1;
+while F0*h < 1500 % 1500 is where brainstem stops phase locking
+    foi = [foi F0*h];
+    h = h + 1;
+end
+
 cfg = [];
 cfg.channel    = 'EP1';
 cfg.method     = 'mtmfft'; % does an FFT across specified time interval
 cfg.output     = 'pow'; % computes the power spectra
 cfg.toilim    = TIME_WINDOW;
-cfg.foi     = [F0 F0*2 F0*3 F0*4 F0*5 F0*6 F0*7]; % frequencies of interest
+cfg.foi     = foi; % frequencies of interest
 cfg.taper      = 'hanning'; % applies hanning taper on window before FFT
 cfg.keeptrials  = 'yes';
 all_data_freq = ft_freqanalysis(cfg, all_data_raw);
@@ -72,5 +86,43 @@ all_data_freq = ft_freqanalysis(cfg, all_data_raw);
 %% export FFT results as .csv
 pow = squeeze(all_data_freq.powspctrm);
 writematrix(pow, strcat(COND, '_harmonics.csv'));
+
+%% compute time-frequency representation
+cfg = [];
+cfg.channel = 'EP1';
+cfg.method = 'wavelet';
+cfg.width = 7;
+cfg.output = 'pow';
+cfg.detrend = 'yes';
+cfg.demean = 'yes';
+cfg.foi = 85:1:1500; % frequencies of interest
+cfg.toi = 'all';
+
+TFR = ft_freqanalysis(cfg, all_data_raw);
+
+% and plot
+cfg = [];
+cfgcfg.baseline = [-.25 0];
+cfg.baselinetype = 'relative';
+figure;
+ft_singleplotTFR(cfg, TFR);
+
+%% plot first ten milliseconds
+cfg = [];
+cfg.channel = 'EP1';
+grandavg = ft_timelockanalysis(cfg, all_data_raw);
+
+cfg = [];
+cfg.channel = 'EP1';
+cfg.xlim = [-.005 .015];
+cfg.parameter = 'avg';
+figure;
+ft_singleplotER(cfg, grandavg);
+
+
+
+
+
+
 
 
--- functions/get_file.m
+++ functions/get_file.m
@@ -1,8 +1,9 @@
-function [fpath_inv fpath_noninv] = get_file(i, cond, directory)
+function [fpath_inv, fpath_noninv] = get_file(i, cond, directory, lp_filt)
 % gets the filepaths for inverted/noninverted pairs of ABR files
 % i: (number) subject ID
 % cond: (char) condition name, as in filename (case sensitive!)
 % directory: (char) where files can be found
+% filt: (number) lowpass filter setting to use
 
     % grab all filenames in directory
     fnames = dir(directory);
@@ -11,7 +12,9 @@ function [fpath_inv fpath_noninv] = get_file(i, cond, directory)
     % and get the ones that match our subject ID and condition
     for f = 1:length(fnames)      
         s = strcat('Subj', num2str(i));
-        if contains(fnames{f}, s) && contains(fnames{f}, cond)
+        lp_filt = num2str(lp_filt);
+        if contains(fnames{f}, s) && contains(fnames{f}, cond) ...
+                && contains(fnames{f}, lp_filt)
             if contains(fnames{f}, 'High')
                 fpath_noninv = strcat(directory, '/', fnames{f});
             elseif contains(fnames{f}, 'Low')
